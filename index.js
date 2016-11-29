40faa7b940e3a1b4e3f3bfcc8e9440b1b391722c
Tyler Long <tyler4long@gmail.com>
2016-11-29 13:13:48 +0800

    Test 500 error

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 
 const app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.status(500).send('server side error');
+});
+app.get('/hello', function (req, res) {
+  res.send('world');
 });
 app.listen(3000);
