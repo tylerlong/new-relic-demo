75f9747045210b5b374412c3b428ce3739cd2988
Tyler Long <tyler4long@gmail.com>
2016-11-29 14:36:23 +0800

    Reload page every 10s

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 app.get('/', function (req, res) {
-  res.status(500).send('server side error');
+  res.status(500).send('Page will be reload soon. <script>setTimeout(function() { location.reload(); }, 10000);</script>');
 });
 app.get('/hello', function (req, res) {
   res.send('world');
